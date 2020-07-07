6390f433ee7a849783381265a7c7b351ecf40bc3
LucasLaibly <original7245@gmail.com>
2020-07-07 18:00:44 -0500

    phase one begin

--- app/hangman.py
+++ app/hangman.py
@@ -9,19 +9,25 @@ class Game:
     def __init__(self, number):
         self.number = number
 
+    # array of words that the user can get assigned
     def guessWord(self, number):
         words = ["pink", "blue", "green", "white"]
         
         return words[number]
 
+    # ask the user for their guess
     def promptForGuess(self):
         newGeuss = str(input("Please select a character guess: "))
 
         self.validateGuess(self.usedLetters, newGeuss)
 
+    # validate the guess to see if it has already been used
     def validateGuess(self, usedLetters, newLetter):
         if( newLetter not in usedLetters ):
             usedLetters.insert(newLetter)
+            return False
+        else:
+            return True
 
     # break guessed word into an array
     # add character at the correct slot
