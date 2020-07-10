705a9727218fef362fd409a42b7bb62fc89f48bb
LucasLaibly <original7245@gmail.com>
2020-07-09 19:57:24 -0500

    main.py logic

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ wordToGuess = ""
 letter = ""
 
 def run():
-    global letter
-    global wordToGuess
-
     # gets the user to enter a number
     while True:
         try:
@@ -15,24 +12,33 @@ def run():
             break
         except:
             print("Not a valid value, try again")
-
+    
     # build the game around the number
     game = hangman.Game(number)
 
-    # retrieve the hidden word
-    wordToGuess  = game.guessWord(number)
+    while True:
+        global letter
+        global wordToGuess
 
-    # prompt for user to begin gameplay
-    letter = game.promptForGuess()
+        action = input("Please select your action. Enter (1) for continue, (2) for making your final guess: ")
 
-    # the end of the game
-    solution = input("What is your final guess? ")
-    result = game.coinFlip(solution)
+        if ( action == "1"):
+            continue
+        else:
+            # the end of the game
+            solution = input("What is your final guess? ")
+            result = game.coinFlip(solution)
+            if(result):
+                print("you win. neato.")
+            else:
+                print("you lost. big sad.")
+                break
 
-    if(result):
-        print("you win. neato.")
-    else:
-        print("you lost. big sad.")
+        # retrieve the hidden word
+        wordToGuess  = game.guessWord(number)
+
+        # prompt for user to begin gameplay
+        letter = game.promptForGuess()
 
 
 if __name__ == "__main__":
